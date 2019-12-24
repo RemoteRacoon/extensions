8c5281a67ca0a09059000d9365a06ca1a3c7891f
RemoteRacoon <remote_racoon@mail.ru>
2019-12-25 00:49:28 +0800

    Trying to solve data load

--- src/extension/controllers/controller.js
+++ src/extension/controllers/controller.js
@@ -9,14 +9,14 @@ class Controller {
     $scope.loadData = () => {
       const requestPage = {
         qTop: $scope.matrix.length,
-        qLeft: 0,
+        qLeft: 10,
         qWidth: 4,
         qHeight: 100
       };
       $scope.backendApi.getData(requestPage)
         .then(data => {
           $scope.matrix.push(...data[0].qMatrix);
-        });
+        }).catch(err => console.log(err));
     };
   }
 }
